058c4e43a6e2f91964677a5411561930e2d90f3a
Nicholas Ray <nrayemail@gmail.com>
2016-05-20 19:13:56 -0600

    fix pagination bugs

--- src/reducers/pagination.js
+++ src/reducers/pagination.js
@@ -1,26 +1,28 @@
-import { PAGE_CHANGE, SEARCH, CHANGE_BOUNDS} from '../actions/index';
+import { PAGE_CHANGE, SEARCH, SEARCH_LOAD, CHANGE_BOUNDS} from '../actions/index';
 
 const INITIAL_STATE = {
   perPage: 18,
   totalCount: 0,
-  offset: 0
+  offset: 0,
+  changing: false
 };
 
 export default function(state = INITIAL_STATE, action) {
-  console.log("from pagination", action.type);
   switch(action.type) {
   case PAGE_CHANGE:
     return Object.assign({}, state, {
-    offset: action.payload.offset
+    offset: action.payload.offset,
+    changing: true
+  });
+  case SEARCH_LOAD:
+    const offset = state.changing ? state.offset : 0;
+    return Object.assign({}, state, {
+    changing: false,
+    offset: offset
   });
   case SEARCH:
     return Object.assign({}, state, {
     totalCount: action.payload.totalCount
-  });
-  case CHANGE_BOUNDS:
-    const offset = action.payload.isPanning ? state.offset : 0;
-    return Object.assign({}, state, {
-    offset: offset
   });
   default:
     return state;
